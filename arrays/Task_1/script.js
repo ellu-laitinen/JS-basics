991ecd9d2a337cbc3ab3a5bc132e4546bffac53d
ElluLaitinen <ellu.laitinen85@gmail.com>
2020-09-10 16:25:42 +0300

    start array tasks

---
+++ arrays/Task_1/script.js
@@ -0,0 +1,37 @@
+/* 	Make an array that has 10 numbers: 
+let numbers = [4, -5, -2, 7, 15, -9, 0, 6, 7, 2];
+    a.	Program prints out the numbers in the array as follows:
+    1. number 4
+    2. number -5
+    ...
+    b.	Program swaps third and seventh number other way around.
+    c.	Program calculates the sum of the numbers and prints out the result.
+    d.	Program calculates the average of the numbers and prints out the result.
+    e.	Program finds out the smallest number and prints it.
+    f.	Program asks one number from the user and then counts how many times that number is in the array. Program prints out the result.
+    g.	Program prints out all the numbers that are in the array before 0.
+    h.	Program counts how many numbers there are after 0 and prints out the result. */
+
+const numbers = [4, -5, -2, 7, 15, -9, 0, 6, 7, 2]
+
+const printNumbers = () => {
+    document.getElementById('numbersArray').innerHTML = numbers
+}
+
+const swapNumbers = () => {
+    const a = numbers[2]
+    numbers[2] = numbers[6];
+    numbers[6] = a
+    document.getElementById('swappedNumbersArray').innerHTML = numbers
+}
+
+const sumOfNumbers = () => {
+
+    const sum = numbers.reduce((a, b) => {
+        return a + b
+    }, 0);
+
+    document.getElementById('sumArray').innerHTML = sum;
+
+
+}
