faf3b2aec069ec7c38833116dadd180e17b7efcc
ElluLaitinen <ellu.laitinen85@gmail.com>
2020-09-10 18:13:15 +0300

    finish math operations

--- mathematical_operators/script.js
+++ mathematical_operators/script.js
@@ -35,3 +35,34 @@ const inCentimeters = () => {
 
     document.getElementById('centimeters').innerHTML = centimeters + " cm."
 }
+
+/* Make a program that calculates body mass index.
+The BMI is defined as the body mass divided by the square of the body height,
+and is universally expressed in units of kg/m2,
+resulting from mass in kilograms and height in metres. Based on result display correct range:
+
+BMI	Weight          Status
+Below 18.5	        Underweight
+18.5 – 24.9	        Normal or Healthy Weight
+25.0 – 29.9	        Overweight
+30.0 and Above	    Obese */
+
+const bmiCalc = () => {
+    const height = document.getElementById('height').value
+    const weight = document.getElementById('weight').value
+
+    const bmi = weight / (height * height)
+    let result = ""
+
+    if (bmi < 18.5) {
+        document.getElementById('result').innerHTML = bmi + " Underweight"
+    } else if (bmi >= 18.5 && bmi < 25.0) {
+        document.getElementById('result').innerHTML = bmi + " Normal or healthy weight"
+    } else if (bmi >= 25 && bmi < 30.0) {
+        document.getElementById('result').innerHTML = bmi + " Overweight"
+    } else if (bmi >= 30.0) {
+        document.getElementById('result').innerHTML = bmi + " Obese"
+    }
+
+    document.getElementById('result').innerHTML
+}
